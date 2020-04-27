fb537928ccd1f8e7508d43fe4c02a033ad210bca
LamasaPace <62271850+LamasaPace@users.noreply.github.com>
2020-04-27 14:06:02 -0500

    Add files via upload

---
+++ Flask Web App/templates/practiceproblems.py
@@ -0,0 +1,18 @@
+#7 to the power of 4
+
+# def square_num():
+# 	print(7**4)
+
+# square_num()
+
+# Problem 3:
+# Given the variables:
+
+planet = "Earth"
+diameter = 12742
+# Use .format() to print the following string:
+
+# The diameter of Earth is 12742 kilometers.
+
+message = f'the diameter of the {planet} is {diameter} kilometers.'
+print (message)
